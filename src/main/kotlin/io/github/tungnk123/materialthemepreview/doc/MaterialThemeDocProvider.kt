993d2a54907c06fe1dd6ce598b2621a2fc87e85a
tungnk123 <doanthanhtungnk123@gmail.com>
2025-11-05 22:56:27 +0700

    Fix bug in document extension

--- src/main/kotlin/io/github/tungnk123/materialthemepreview/doc/MaterialThemeDocProvider.kt
+++ src/main/kotlin/io/github/tungnk123/materialthemepreview/doc/MaterialThemeDocProvider.kt
@@ -7,25 +7,21 @@ import io.github.tungnk123.materialthemepreview.service.ThemeIndexService
 import org.jetbrains.kotlin.psi.KtElement
 
 class MaterialThemeDocProvider : DocumentationProvider {
+
     override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
-        val kt = originalElement as? KtElement ?: return null
+        val kt = (originalElement as? KtElement) ?: return null
         val match = KtMatchers.matchMaterialThemeExpr(kt) ?: return null
         val idx = kt.project.getService(ThemeIndexService::class.java)
 
         return when (match.kind) {
-            KtMatchers.Match.Kind.COLOR ->
-                idx.getColor(match.name)?.let {
-                    "<b>Material color:</b> ${match.name}<br/><code>$it</code>"
-                }
-            KtMatchers.Match.Kind.TYPO ->
-                idx.getTextStyle(match.name)?.let {
-                    "<b>Text style:</b> ${match.name}<br/>" +
-                            "size=${it.sizeSp ?: "?"}sp, lineHeight=${it.lineHeightSp ?: "?"}sp"
-                }
-            KtMatchers.Match.Kind.SHAPE ->
-                idx.getShape(match.name)?.let {
-                    "<b>Shape:</b> ${match.name}<br/>$it"
-                }
+            KtMatchers.Match.Kind.COLOR -> idx.getColor(match.name)
+                ?.let { "<b>Material color:</b> ${match.name}<br/><code>$it</code>" }
+
+            KtMatchers.Match.Kind.TYPO -> idx.getTextStyle(match.name)?.let {
+                "<b>Text style:</b> ${match.name}<br/>size=${it.sizeSp ?: "?"}sp, lineHeight=${it.lineHeightSp ?: "?"}sp"
+            }
+
+            KtMatchers.Match.Kind.SHAPE -> idx.getShape(match.name)?.let { "<b>Shape:</b> ${match.name}<br/>$it" }
         }
     }
 }
