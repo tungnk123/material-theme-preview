81b036db6354c9a6104ce6711b579d663a1986dd
tungnk123 <doanthanhtungnk123@gmail.com>
2025-11-05 23:11:37 +0700

    Update MaterialColorLookup

--- src/main/kotlin/io/github/tungnk123/materialthemepreview/complete/MaterialColorLookup.kt
+++ src/main/kotlin/io/github/tungnk123/materialthemepreview/complete/MaterialColorLookup.kt
@@ -4,8 +4,23 @@ import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 
 object MaterialColorLookup {
-    fun build(name: String, hex: String): LookupElement =
-        LookupElementBuilder.create(name)
-            .withTypeText(hex, true)
-            .withIcon(MaterialColorIcon(hex))
+    fun build(colorName: String, hexColor: String): LookupElement {
+        return LookupElementBuilder.create(colorName)
+            .withPresentableText(colorName)
+            .withTypeText(hexColor, true)
+            .withTailText("  MaterialTheme.colorScheme", true)
+            .withItemTextItalic(false)
+            .withStrikeoutness(false)
+            .withIcon(MaterialColorIcon(hexColor))
+            .withTypeIconRightAligned(true)
+            .withInsertHandler { context, _ ->
+                val doc = context.document
+                val offset = context.tailOffset
+                val textAfter = doc.charsSequence.getOrNull(offset)
+                if (textAfter != ' ' && textAfter != ',' && textAfter != ')') {
+                    doc.insertString(offset, " ")
+                    context.editor.caretModel.moveToOffset(offset + 1)
+                }
+            }
+    }
 }
