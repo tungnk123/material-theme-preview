7d1a530f7815cfb951bfb228dfa2f32feb4834fb
tungnk123 <doanthanhtungnk123@gmail.com>
2025-11-06 00:14:25 +0700

    Refactor logic in  MaterialThemeInlayProvider

--- src/main/kotlin/io/github/tungnk123/materialthemepreview/inlay/MaterialThemeInlayProvider.kt
+++ src/main/kotlin/io/github/tungnk123/materialthemepreview/inlay/MaterialThemeInlayProvider.kt
@@ -3,16 +3,10 @@ package io.github.tungnk123.materialthemepreview.inlay
 import com.intellij.codeInsight.hints.*
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
-import com.intellij.ui.JBColor
-import com.intellij.util.ui.UIUtil
 import io.github.tungnk123.materialthemepreview.KtMatchers
+import io.github.tungnk123.materialthemepreview.complete.MaterialColorIcon
 import io.github.tungnk123.materialthemepreview.service.ThemeIndexService
 import org.jetbrains.kotlin.psi.KtElement
-import java.awt.Color
-import java.awt.Graphics
-import java.awt.image.BufferedImage
-import javax.swing.Icon
-import javax.swing.ImageIcon
 import javax.swing.JComponent
 import javax.swing.JPanel
 
@@ -29,72 +23,64 @@ class MaterialThemeInlayProvider : InlayHintsProvider<NoSettings> {
 
     override fun createSettings(): NoSettings = NoSettings()
 
-    override fun createConfigurable(settings: NoSettings): ImmediateConfigurable {
-        return object : ImmediateConfigurable {
-            override fun createComponent(listener: ChangeListener): JComponent {
-                return JPanel()
-            }
-        }
+    override fun createConfigurable(settings: NoSettings): ImmediateConfigurable = object : ImmediateConfigurable {
+        override fun createComponent(listener: ChangeListener): JComponent = JPanel()
     }
 
     override fun getCollectorFor(
-        file: com.intellij.psi.PsiFile,
-        editor: Editor,
-        settings: NoSettings,
-        sink: InlayHintsSink
+        file: com.intellij.psi.PsiFile, editor: Editor, settings: NoSettings, sink: InlayHintsSink
     ): InlayHintsCollector {
-        val index = file.project.getService(ThemeIndexService::class.java)
+        val themeIndex = file.project.getService(ThemeIndexService::class.java)
 
         return object : FactoryInlayHintsCollector(editor) {
             override fun collect(element: PsiElement, editor: Editor, sink: InlayHintsSink): Boolean {
-                val kt = element as? KtElement ?: return true
-                val match = KtMatchers.matchMaterialThemeExpr(kt) ?: return true
+                val ktElement = element as? KtElement ?: return true
+                val match = KtMatchers.matchMaterialThemeExpr(ktElement) ?: return true
 
                 when (match.kind) {
                     KtMatchers.Match.Kind.COLOR -> {
-                        val hex = index.getColor(match.name) ?: return true
-                        val pres = factory.seq(
-                            factory.smallText("  "),
-                            factory.roundWithBackground(factory.smallText(hex)),
-                            factory.smallText(" "), factory.icon(colorIcon(hex))
+                        val hex = themeIndex.getColor(match.name) ?: return true
+                        val presentation = factory.seq(
+                            factory.text("  "),
+                            factory.roundWithBackground(factory.text(hex)),
+                            factory.text(" "),
+                            factory.icon(MaterialColorIcon(hex))
                         )
-                        sink.addInlineElement(element.textRange.endOffset, false, pres, false)
+                        sink.addInlineElement(element.textRange.endOffset, false, presentation, false)
                     }
+
                     KtMatchers.Match.Kind.TYPO -> {
-                        val ts = index.getTextStyle(match.name) ?: return true
+                        val textStyle = themeIndex.getTextStyle(match.name) ?: return true
                         val label = buildString {
-                            append("size="); append(ts.sizeSp?.toString() ?: "?"); append("sp")
-                            if (ts.lineHeightSp != null) { append(", lh="); append(ts.lineHeightSp); append("sp") }
+                            append("size=")
+                            append(textStyle.sizeSp?.toString() ?: "?")
+                            append("sp")
+                            if (textStyle.lineHeightSp != null) {
+                                append(", line-height=")
+                                append(textStyle.lineHeightSp)
+                                append("sp")
+                            }
                         }
                         sink.addInlineElement(
-                            element.textRange.endOffset, false,
-                            factory.roundWithBackground(factory.smallText(label)), false
+                            offset = element.textRange.endOffset,
+                            relatesToPrecedingText = false,
+                            presentation = factory.roundWithBackground(factory.text(label)),
+                            placeAtTheEndOfLine = false
                         )
                     }
+
                     KtMatchers.Match.Kind.SHAPE -> {
-                        val s = index.getShape(match.name) ?: return true
+                        val shapeText = themeIndex.getShape(match.name) ?: return true
                         sink.addInlineElement(
-                            element.textRange.endOffset, false,
-                            factory.roundWithBackground(factory.smallText(s)), false
+                            element.textRange.endOffset,
+                            false,
+                            factory.roundWithBackground(factory.text(shapeText)),
+                            false
                         )
                     }
                 }
                 return true
             }
         }
-    }
-
-    private fun colorIcon(hex: String): Icon {
-        val c: Color = try {
-            JBColor.decode(hex)
-        } catch (_: Throwable) {
-            JBColor.GRAY
-        }
-        val img = UIUtil.createImage(10, 10, BufferedImage.TYPE_INT_ARGB)
-        val g: Graphics = img.graphics
-        g.color = c
-        g.fillRoundRect(0, 0, 10, 10, 4, 4)
-        g.dispose()
-        return ImageIcon(img)
     }
 }
